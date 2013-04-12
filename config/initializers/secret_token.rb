89c4dfa4bad8052918ae76b86cd9cdd075c5afe6
kmanpdx <kenny@netravine.com>
2013-04-12 11:49:38 -0700

    First Commit for Glacier app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glacier::Application.config.secret_token = '623865a884d12d3ee01b06249cd7dbd509691f33353942d0bbd1bd5cbe58894f1afbade4262a2e7e4df52d04923034d0df3d5cfc29366b8c090921fe418341d2'
